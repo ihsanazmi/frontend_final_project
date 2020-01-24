99ef15ace45fc95bbac31038f570d84c30333c51
ihsanazmi <ihsanazmiii@gmail.com>
2020-01-24 11:14:01 +0700

    deploy heroku 1

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:2018'
+    // baseURL: 'http://localhost:2018'
+    baseURL: 'https://backend-komputer-shop.herokuapp.com/'
 })
